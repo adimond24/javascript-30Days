32ad90f4e8a46456a0f7d05d559281c7cbb34192
adimond24 <adimond24@s.tooeletech.edu>
2024-05-20 18:18:06 -0600

    first day

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+let age = 6
+let firstname = Alison 
+let lastname = lovell
+let country = china
+/* let firstname = Amelia
+let lastname = Diiiiiiamd
+let isMarried = false
+let country = mexico
+let age = 24 */
+//let firstname = Amelia let lastname = Diiiiiiamd let isMarried = false let country = mexico let age = 24//
+let myAge = 17
+let yourAge = 16
+
+console.log('I am' myAge 'years old')
+console.log('You are' yourAge 'years old' ) 
